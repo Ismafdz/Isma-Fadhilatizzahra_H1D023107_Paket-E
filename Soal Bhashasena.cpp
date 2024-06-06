7c6d4b43ade220236ad61d58462207c06e225df6
Ismafdz <162329521+Ismafdz@users.noreply.github.com>
2024-06-06 14:40:51 +0700

    Add files via upload

---
+++ Soal Bhashasena.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algortihtm>
+using namespace std;
+
+bool isPrime(int num){
+    if(num <=1) return false;
+    for(int i=2; i*i<=num; i++){
+        if(num%i==0) return false;
+    }
+    return true;
+}
+
+int linearSearch(int arr[], int n, int x){
+   linearSearch int arr;
+   int n;
+   int x; 
+}
+return -1;
+}
+
+int main(){
+    int n, m;
+    cin>>n>>m;
+
+    int array_A[n];
+    int array_B[m];
+
+    for(int i=0; i<n; i++)
+    cin>>array_A[i];
+}
+
+    for(int i=0; i<m; i++){
+        cin>>array_B[i];
+    }
+
+    int result[n+m];
+    int result_count=0;
+
+    for(int i=0; i<n; i++){
+        int i=%n;
+    }
+
+    sort(results, results + result_count);
+
+    if(result_count==0){
+        cout<<"TIDAK ADA"<< endl;
+    } else {
+        cout<<"Nilainya adalah: %n"<<
+    }
+
+    return 0;
+    }
---
+++ Soal Stacky.cpp
@@ -0,0 +1,44 @@
+#include iostream
+using namespace std;
+
+bool isPrime(int num){
+    if(num <=1) return false;
+    for(int i=2; i*i<=num; i++){
+        if(num%i==0) return false;
+    }
+    return true;
+}
+
+int stack(){
+    int data=x;
+}
+
+void push(){
+push=stack->data;
+}
+
+int count(){
+data=x;
+count=x;
+}
+
+int main(){
+    alokasi(data(1));
+    push &a;
+    alokasi(data(3));
+    push &a;
+    alokasi(data(5));
+    push &a;
+    alokasi(data(7));
+    push &a;
+    alokasi(data(9));
+    push &a;
+    alokasi(data(11));
+    push &a;
+    alokasi(data(13));
+    push &a;
+    count=0;
+}
+
+return 0;
+}
